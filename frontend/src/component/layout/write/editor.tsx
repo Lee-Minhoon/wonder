0e5cf1b4916bbaf3ad46b57bafc049953366fe2c
Lee Minhoon <hackids@naver.com>
2021-11-15 02:13:01 +0900

    Toast UI Plugin 적용

--- frontend/src/component/layout/write/editor.tsx
+++ frontend/src/component/layout/write/editor.tsx
@@ -1,10 +1,17 @@
-
+import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+import { Editor as ToastEditor } from '@toast-ui/react-editor';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 
 export default function Editor() {
     return (
-        <div id="app">
-            <div id="editor"></div>
-            <pre id="output"></pre>
-        </div>
+        <ToastEditor
+            initialValue="hello react editor world!"
+            previewStyle="vertical"
+            height="600px"
+            initialEditType="markdown"
+            useCommandShortcut={true}
+            plugins={[colorSyntax]}
+        />
     )
 }
--- frontend/src/component/layout/write/index.tsx
+++ frontend/src/component/layout/write/index.tsx
@@ -3,21 +3,31 @@ import Right from "component/common/right";
 import { useRef, useEffect } from 'react';
 import styles from './styles.module.scss';
 import dynamic from 'next/dynamic';
-import '@toast-ui/editor/dist/toastui-editor.css';
 
-const Editor = dynamic(() => import('@toast-ui/react-editor').then(m => m.Editor), { ssr: false });
-
+const Editor = dynamic(() => import('./editor'), { ssr: false });
 
 export default function Write() {
     return (
         <div>
-            <Editor
-                initialValue="hello react editor world!"
-                previewStyle="vertical"
-                height="600px"
-                initialEditType="markdown"
-                useCommandShortcut={true}
-            />
+            <Editor />
         </div>
     )
 }
+
+// import '@toast-ui/editor/dist/toastui-editor.css';
+// import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+// const Editor = dynamic(() => import('@toast-ui/react-editor').then(m => m.Editor), { ssr: false });
+// const colorSyntax = () => dynamic(() => import('@toast-ui/editor-plugin-color-syntax').then(m => m.default), { ssr: false });
+
+// export default function Write() {
+//     return (
+//         <Editor
+//             initialValue="hello react editor world!"
+//             previewStyle="vertical"
+//             height="600px"
+//             initialEditType="markdown"
+//             useCommandShortcut={true}
+//             plugins={[colorSyntax]}
+//         />
+//     )
+// }
